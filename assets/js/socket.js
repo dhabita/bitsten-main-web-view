710716249eb216b76fed5f5cc9f731a22f06be43
kindcow <49510661+dhabita@users.noreply.github.com>
2021-07-27 11:23:59 +0700

    right-number

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -165,11 +165,11 @@ update_global_price(e.market_show,e.bid);
 var tddata = '\
 <td><i class="icon ion-md-star"></i> '+e.market_show.toUpperCase()+'</td>\
 <td class=" hide-xs"><img src="https://f1.bitsten.com/assets/images/logo/'+coin+'.png"  width="20px" alt="'+coin+'"> '+coin.toUpperCase()+'</td>\
-<td>'+number_format(e.bid)+'</td>\
-<td class="'+col+'">'+number_format(change,2)+'%</td>\
-<td class="  hide-xs">'+number_format(e.high)+'</td>\
-<td class=" hide-xs ">'+number_format(e.low)+'</td>\
-<td class="  hide-xs " >'+number_format(e.volume)+'</td>';
+<td class=" text-right  ">'+number_format(e.bid)+'</td>\
+<td class=" text-right '+col+'">'+number_format(change,2)+'%</td>\
+<td class=" text-right hide-xs">'+number_format(e.high)+'</td>\
+<td class="text-right  hide-xs ">'+number_format(e.low)+'</td>\
+<td class=" text-right hide-xs " >'+number_format(e.volume)+'</td>';
 
 var tddatam = '\
 <td   class="col-5 pl-4"><i class="icon ion-md-star"></i> '+e.market_show.toUpperCase()+'</td>\
